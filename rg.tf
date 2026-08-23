b948f20fdf0d25d30e1db8c88cf0ddbbb6b81c4b
Gurusundaram Balasubramaniyan <120612852+GuruSundaram1@users.noreply.github.com>
2026-08-23 23:16:59 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,1 +1,15 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.4.0"
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grp"
+  location = "North Europe"
+}
