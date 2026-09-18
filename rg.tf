8b033b12e1b71352c0960fae5d1da97b86771419
GuruSundaram1 <gurubca16@gmail.com>
2026-09-18 20:19:09 +0530

    modified profile

--- rg.tf
+++ rg.tf
@@ -43,11 +43,11 @@ resource "azurerm_storage_account" "insecure" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # 🔴 KICS will flag this
-  allow_nested_items_to_be_public = true
+  https_traffic_only_enabled      = true
+  allow_nested_items_to_be_public = false
 
   network_rules {
-    default_action = "Allow" # 🔴 KICS will flag this too
+    default_action = "Deny"
     bypass         = ["AzureServices"]
   }
 }
